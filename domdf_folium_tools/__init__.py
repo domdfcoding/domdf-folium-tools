c7fd93f2068e56607e0e3b32a5dff23785be9e40
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-04-19 11:07:47 +0100

    Bump version v0.1.0b8 -> v0.1.0b9

--- domdf_folium_tools/__init__.py
+++ domdf_folium_tools/__init__.py
@@ -39,7 +39,7 @@ __all__ = ["Coordinates", "FeatureCollection", "embed_styles", "set_branca_rando
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2026 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0b8"
+__version__: str = "0.1.0b9"
 __email__: str = "dominic@davis-foster.co.uk"
 
 
