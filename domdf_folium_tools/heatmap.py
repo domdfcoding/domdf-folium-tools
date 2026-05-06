6d43ac689fbafb8936be780ae1cdec0e0cdcab9e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-05-06 09:05:45 +0100

    WIP heatmap plugin.

---
+++ domdf_folium_tools/heatmap.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python3
+#
+#  heatmap.py
+"""
+Cumulative heatmaps – data from all previous time windows is included in the "current" view.
+"""
+#
+#  Copyright © 2026 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#  OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# stdlib
+from typing import Optional, Union
+
+# 3rd party
+import folium.plugins
+from branca.utilities import none_max, none_min
+from folium.elements import JSCSSMixin
+from folium.map import Layer
+from folium.template import Template
+
+# this package
+from domdf_folium_tools import __version__
+
+__all__ = ["HeatLayerWithTime", "HeatMapWithTime"]
+
+
+class HeatMapWithTime(JSCSSMixin, Layer):
+	"""
+	Create a ``HeatMapWithTime`` layer
+
+	:param data: The points you want to plot. Nested list of points in the form ``[lat, lng]`` or ``[lat, lng, weight]``.
+		The outer list corresponds to the various time steps in sequential order.
+		Weight is in ``(0, 1]`` range and defaults to ``1`` if not specified for a point.
+	:param index: Index giving the label (or timestamp) of the elements of data.
+		Should have the same length as data, or is replaced by a simple count if not specified.
+	:param name: The name of the Layer, as it will appear in LayerControls.
+	:param radius: The radius used around points for the heatmap.
+	:param blur: Blur strength used for the heatmap. Must be between 0 and 1.
+	:param min_opacity: The minimum opacity for the heatmap.
+	:param max_opacity: The maximum opacity for the heatmap.
+	:param scale_radius: Scale the radius of the points based on the zoom level.
+	:param gradient: Mapping of point density values to colours.
+		Colour can be a name (``'red'``), RGB values (``'rgb(255,0,0)'``) or a hex number (``'#FF0000'``).
+	:param use_local_extrema: Defines whether the heatmap uses a global extrema set found from the input data
+		OR a local extrema (the maximum and minimum of the currently displayed view).
+	:param auto_play: Automatically play the animation across time.
+	:param display_index: Display the index (usually time) in the time control.
+	:param index_steps: Steps to take in the index dimension between animation steps.
+	:param min_speed: Minimum fps speed for animation.
+	:param max_speed: Maximum fps speed for animation.
+	:param speed_step: Step between different fps speeds on the speed slider.
+	:param position: Position string for the time slider. Format: 'bottom/top'+'left/right'.
+	:param overlay: Adds the layer as an optional overlay (True) or the base layer (False).
+	:param control: Whether the Layer will be included in LayerControls.
+	:param show: Whether the layer will be shown on opening.
+
+	"""
+
+	_template = Template(
+			"""
+        {% macro script(this, kwargs) %}
+
+            var times = {{this.times}};
+
+            {{this._parent.get_name()}}.timeDimension = L.timeDimension(
+                {times : times, currentTime: new Date(1)}
+            );
+
+            var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
+                autoPlay: {{this.auto_play}},
+                backwardButton: {{this.backward_button}},
+                displayDate: {{this.display_index}},
+                forwardButton: {{this.forward_button}},
+                limitMinimumRange: {{this.limit_minimum_range}},
+                limitSliders: {{this.limit_sliders}},
+                loopButton: {{this.loop_button}},
+                maxSpeed: {{this.max_speed}},
+                minSpeed: {{this.min_speed}},
+                playButton: {{this.play_button}},
+                playReverseButton: {{this.play_reverse_button}},
+                position: "{{this.position}}",
+                speedSlider: {{this.speed_slider}},
+                speedStep: {{this.speed_step}},
+                styleNS: "{{this.style_NS}}",
+                timeSlider: {{this.time_slider}},
+                timeSliderDragUpdate: {{this.time_slider_drag_update}},
+                timeSteps: {{this.index_steps}}
+                })
+                .addTo({{this._parent.get_name()}});
+
+                var {{this.get_name()}} = new TDHeatmap({{this.data}},
+                {heatmapOptions: {
+                        radius: {{this.radius}},
+                        blur: {{this.blur}},
+                        minOpacity: {{this.min_opacity}},
+                        maxOpacity: {{this.max_opacity}},
+                        scaleRadius: {{this.scale_radius}},
+                        useLocalExtrema: {{this.use_local_extrema}},
+                        defaultWeight: 1,
+                        {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
+                    }
+                });
+
+        {% endmacro %}
+        """,
+			)
+
+	default_js = [
+			(
+					"iso8601",
+					"https://cdn.jsdelivr.net/npm/iso8601-js-period@0.2.1/iso8601.min.js",
+					),
+			(
+					"leaflet.timedimension.min.js",
+					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.min.js",
+					),
+			(
+					"heatmap.min.js",
+					"https://cdn.jsdelivr.net/gh/python-visualization/folium/folium/templates/pa7_hm.min.js",
+					),
+			(
+					"leaflet-heatmap.js",
+					"https://cdn.jsdelivr.net/gh/python-visualization/folium/folium/templates/pa7_leaflet_hm.min.js",
+					),
+			(
+					"domdf_folium_tools_heatmap_js",
+					f"https://cdn.jsdelivr.net/gh/domdfcoding/domdf-folium-tools@v{__version__}/domdf_folium_tools/heatmap.min.js"
+					),
+			]
+	default_css = [
+			(
+					"leaflet.timedimension.control.min.css",
+					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.control.css",
+					),
+			]
+
+	def __init__(
+			self,
+			data: list[list[Union[tuple[float, float, float], tuple[float, float]]]],
+			index: Optional[list] = None,
+			name: Optional[str] = None,
+			radius: int = 15,
+			blur: float = 0.8,
+			min_opacity: float = 0,
+			max_opacity: float = 0.6,
+			scale_radius: bool = False,
+			gradient: Optional[dict[float, str]] = None,
+			use_local_extrema: bool = False,
+			auto_play: bool = False,
+			display_index: bool = True,
+			index_steps: int = 1,
+			min_speed: float = 0.1,
+			max_speed: float = 10,
+			speed_step: float = 0.1,
+			position: str = "bottomleft",
+			overlay: bool = True,
+			control: bool = True,
+			show: bool = True,
+			):
+		super().__init__(name=name, overlay=overlay, control=control, show=show)
+		self._name = "HeatMap"
+		self._control_name = self.get_name() + "Control"
+
+		# Input data.
+		self.data = data
+		self.index = (index if index is not None else [str(i) for i in range(1, len(data) + 1)])
+		if len(self.data) != len(self.index):
+			raise ValueError("Input data and index are not of compatible lengths.", )  # noqa
+		self.times = list(range(1, len(data) + 1))
+
+		# Heatmap settings.
+		self.radius = radius
+		self.blur = blur
+		self.min_opacity = min_opacity
+		self.max_opacity = max_opacity
+		self.scale_radius = "true" if scale_radius else "false"
+		self.use_local_extrema = "true" if use_local_extrema else "false"
+		self.gradient = gradient
+
+		# Time dimension settings.
+		self.auto_play = "true" if auto_play else "false"
+		self.display_index = "true" if display_index else "false"
+		self.min_speed = min_speed
+		self.max_speed = max_speed
+		self.position = position
+		self.speed_step = speed_step
+		self.index_steps = index_steps
+
+		# Hard coded defaults for simplicity.
+		self.backward_button = "true"
+		self.forward_button = "true"
+		self.limit_sliders = "true"
+		self.limit_minimum_range = 5
+		self.loop_button = "true"
+		self.speed_slider = "true"
+		self.time_slider = "true"
+		self.play_button = "true"
+		self.play_reverse_button = "true"
+		self.time_slider_drag_update = "false"
+		self.style_NS = "leaflet-control-timecontrol"
+
+	def _get_self_bounds(self):
+		"""
+		Computes the bounds of the object itself (not including it's children)
+		in the form [[lat_min, lon_min], [lat_max, lon_max]].
+
+		"""
+		bounds = [[None, None], [None, None]]
+		for point in self.data:
+			bounds = [
+					[
+							none_min(bounds[0][0], point[0]),
+							none_min(bounds[0][1], point[1]),
+							],
+					[
+							none_max(bounds[1][0], point[0]),
+							none_max(bounds[1][1], point[1]),
+							],
+					]
+		return bounds
+
+
+class HeatLayerWithTime(folium.plugins.HeatMapWithTime):
+
+	def __init__(
+			self,
+			data,
+			index=None,
+			name=None,
+			radius=25,
+			blur=15,
+			min_opacity=0.05,
+			max_opacity=0.6,
+			scale_radius=False,
+			gradient=None,
+			use_local_extrema=False,
+			auto_play=False,
+			display_index=True,
+			index_steps=1,
+			min_speed=0.1,
+			max_speed=10,
+			speed_step=0.1,
+			position="bottomleft",
+			overlay=True,
+			control=True,
+			show=True,  # TODO: max
+			):
+		super().__init__(
+				data=data,
+				index=index,
+				name=name,
+				radius=radius,
+				blur=blur,
+				min_opacity=min_opacity,
+				max_opacity=max_opacity,
+				scale_radius=scale_radius,
+				gradient=gradient,
+				use_local_extrema=use_local_extrema,
+				auto_play=auto_play,
+				display_index=display_index,
+				index_steps=index_steps,
+				min_speed=min_speed,
+				max_speed=max_speed,
+				speed_step=speed_step,
+				position=position,
+				overlay=overlay,
+				control=control,
+				show=show,
+				)
+
+	default_js = [
+			(
+					"iso8601",
+					"https://cdn.jsdelivr.net/npm/iso8601-js-period@0.2.1/iso8601.min.js",
+					),
+			(
+					"leaflet.timedimension.min.js",
+					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.min.js",
+					),
+			(
+					"leaflet-heat.js",
+					"https://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js",  # TODO: jsdelivr
+					),
+			("nhle_heatmap", "static/js/heatmap.js"),
+			]
+	default_css = [
+			(
+					"leaflet.timedimension.control.min.css",
+					"https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.control.css",
+					),
+			]
+
+	_template = Template(
+			"""
+        {% macro script(this, kwargs) %}
+
+            var times = {{this.times}};
+
+            {{this._parent.get_name()}}.timeDimension = L.timeDimension(
+                {times : times, currentTime: new Date(1)}
+            );
+
+            var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
+                autoPlay: {{this.auto_play}},
+                backwardButton: {{this.backward_button}},
+                displayDate: {{this.display_index}},
+                forwardButton: {{this.forward_button}},
+                limitMinimumRange: {{this.limit_minimum_range}},
+                limitSliders: {{this.limit_sliders}},
+                loopButton: {{this.loop_button}},
+                maxSpeed: {{this.max_speed}},
+                minSpeed: {{this.min_speed}},
+                playButton: {{this.play_button}},
+                playReverseButton: {{this.play_reverse_button}},
+                position: "{{this.position}}",
+                speedSlider: {{this.speed_slider}},
+                speedStep: {{this.speed_step}},
+                styleNS: "{{this.style_NS}}",
+                timeSlider: {{this.time_slider}},
+                timeSliderDragUpdate: {{this.time_slider_drag_update}},
+                timeSteps: {{this.index_steps}}
+                })
+                .addTo({{this._parent.get_name()}});
+
+                var {{this.get_name()}} = new TDHeatLayer(heatmapData,
+                {heatmapOptions: {
+                        radius: {{this.radius}},
+                        blur: {{this.blur}},
+                        minOpacity: {{this.min_opacity}},
+                        // maxOpacity: {{this.max_opacity}},
+                        // scaleRadius: {{this.scale_radius}},
+                        // useLocalExtrema: {{this.use_local_extrema}},
+                        // defaultWeight: 1,
+                        {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
+                    }
+                });
+
+        {% endmacro %}
+        """,
+			)
